08ec7ed2911c12f9e7bbe5d9fafb455986714b50
ukmiecik <kmiecik.ugo@gmail.com>
2026-07-14 20:58:25 +0200

    hingeloss function

--- python/utils/cost_fun.py
+++ python/utils/cost_fun.py
@@ -1,5 +1,8 @@
-def logloss (x, y) :
+def logloss (x,y,n) :
     return 1
 
-def hingeloss(x,y) : 
-    return 1 
+def hingeloss(x,y,n) :
+    sum = 0
+    for i in range(n) :
+        sum = sum + max(0, 1-x[i]*y[i])
+    return sum
