06d1db1508ccae49453c31a17f692a9eff6f2b97
croumache <croumache@gmail.com>
2026-07-18 20:43:01 +0200

    encapsulation and Readme

--- python/README.md
+++ python/README.md
@@ -1,15 +1,15 @@
 # Perceptron in python for prototyping
 
+to see further information : read the report
 
 ## Goal 
-the main goal is to structure our idees, and build the prototype for the project we will later implement in assembly.
+The main goal is to structure our idees, and build the prototype for the project we will later implement in assembly.
+The code has to be simple enough to be a proper prototype for the assemby version.
 
-## Activation function
 
-In this project we decided to try two different activation function : 
-- step function
-- sigmoïd
-
-## Cost function 
-To find our parameters we will find the minimum of the cost function (the log loss function in our case)
+## Structure
+The code is divided in three parts : 
+- utils/activation_fun.py implements the simple step function
+- utils/optimization.py implements the gradient descent as well as the gradient of the perceptron loss function
+- main.py implements the training algorithm using a set of known samples as well as the perceptron function that can classify previouly unseen data (and seen since our case is a bit artificial)
 
--- python/main.py
+++ python/main.py
@@ -1,18 +1,18 @@
 import random
 import numpy as np
-from utils import optimization as opti
-from utils import activation_fun as acti
+from utils import optimization as opt
+from utils import activation_fun as act
+
+"""
+set of training data
 """
 X1 = np.array ([0,0,1,1]) # set of sample for the first input
 X2 = np.array ([0,1,0,1]) # set of sample for the second input
 Y = np.array ([0,0,0,1]) # set of expected result
+
 """
-
-
-X1 = np.array ([-1,-1,1,1]) # set of sample for the first input
-X2 = np.array ([-1,1,-1,1]) # set of sample for the second input
-Y = np.array ([-1,-1,-1,1]) # set of expected result
-
+global constants
+"""
 nmax = 10000
 h = 0.001
 tol = 0.001
@@ -20,28 +20,35 @@ n_sample = 4
 
 
 """
-    - X known data (training data)
-    - Y expected result
-    returns the optimal parameters
+- X known data (training data)
+- Y expected result
+returns the optimal parameters
 """
+
 def training (X1, X2, Y) : 
+    w1 = random.random()
+    w2 = random.random()
+    b = random.random()
 
-    w1 = random.random()*10
-    w2 = random.random()*10
-    b = random.random()*10
+    print("random initial parameters values : ", w1, w2, b)
 
-    print("les données aléatoires sont ", w1, w2, b)
-
-    return opti.gradient_descent([w1, w2, b], tol, nmax, h, lambda C: opti.dhingeloss(C, n_sample, X1, X2, Y))
+    return opt.gradient_descent([w1, w2, b], tol, nmax, h, lambda C: opt.dhingeloss(C, n_sample, X1, X2, Y))
 
 """
-returns the result of the perceptron for entry x1 and x2. The perceptron is trained for data X1, X2, Y 
+- X known data (training data)
+- Y expected result
+- x entry
+returns the result of the perceptron for entry x1 and x2. 
 """
-def perceptron (X1, X2, Y, x1, x2) : 
+def perceptron (X1, X2, Y, x1, x2) :
+    X1 = 2*X1-1
+    X2 = 2*X2-1
+    Y = 2*Y-1 
+    x1 = 2*x1-1
+    x2 = 2*x2-1
     w1, w2, b = training(X1, X2, Y)
-    print("les données après training sont",w1, w2, b)
-    z = opti.linear_model(w1,w2, b, x1, x2)
-    print("z vaut", z)
-    return acti.step_fun(z)
+    print("parameters values after training : ",w1, w2, b)
+    z = opt.linear_model(w1,w2, b, x1, x2)
+    print("z =", z)
+    return act.step_fun(z)
 
-print(perceptron (X1, X2, Y, 1, 1))
